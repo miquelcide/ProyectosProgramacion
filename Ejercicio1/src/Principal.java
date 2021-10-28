96876647c97edef8bb426a29ad61301eebb4b442
Miquel <miguel.beltran@cide.es>
2021-10-28 15:25:15 +0200

    version 2 del programa con la comparacion 1=1. y funciona.

--- Ejercicio1/src/Principal.java
+++ Ejercicio1/src/Principal.java
@@ -3,5 +3,11 @@ public class Principal {
     public static void main(String[] args) {
         System.out.println("Ejercicio 1 programacion");
         System.out.println(1+1);
+        System.out.println("Miquel");
+        if (1==1){
+            System.out.println("son iguales");
+        }else{
+            System.out.println("son diferentes");
+        }
     }
 }
